a931ea8315789259b0f2f998d2a29e1719060c65
Mahir <mahir.suljic@fet.ba>
2024-10-11 15:56:07 +0200

    Skripta za inicijalizaciju baze podataka studentska sluzba

--- Vjezbe/init/README.md
+++ Vjezbe/init/README.md
@@ -1,2 +1,1 @@
-Prije izvršenja kasnije navedene komande potrebno je u fajlu `init.sql` promijeniti ime baze podataka (na dva mjesta gdje je naznačeno) na onaj način naveden u pripremi za vježbe.
-Za inicijalizaciju glavne baze podataka izvršiti komandu `. ./init.sh` unutar ove direktorije (bitno je da se na mjestu izvršavanja komande nalaze fajl `init.sh`, direktorija `tmp` i svi .unl fajlovi unutar nje).
+Za inicijalizaciju glavne baze podataka izvršiti komandu `. ./init.sh Ime Prezime` unutar ove direktorije (bitno je da se na mjestu izvršavanja komande nalaze fajl `init.sh`, direktorija `fetdata` i svi .unl fajlovi unutar nje).
--- Vjezbe/init/init.sh
+++ Vjezbe/init/init.sh
@@ -1,3 +1,36 @@
-sudo cp -r ./tmp/ /var/lib/mysql/
-mysql --local-infile=1 -u root -p < init.sql
-sudo rm -r /var/lib/mysql/tmp
+if [[ $# -eq 2 ]]; 
+then
+  file=/etc/mysql/my.cnf
+  if grep -Fxq "[mysqld]" $file 
+  then
+    if grep -Fq "secure_file_priv=" $file 
+    then
+      if ! grep -Fxq "secure_file_priv=/var/lib/mysql-files/" $file 
+      then
+        sudo sed -i -r "s/secure_file_priv=.*/secure_file_priv=\/var\/lib\/mysql-files\//" $file
+        sudo service mysql restart
+        sudo sed -i "/secure_file_priv=\/var\/lib\/mysql-files\//d" $file
+      fi
+    else
+      sudo sed -i "/\[mysqld\]/a secure_file_priv=\/var\/lib\/mysql-files\/" $file
+      sudo service mysql restart
+      sudo sed -i "/secure_file_priv=\/var\/lib\/mysql-files\//d" $file
+    fi
+  else
+    sudo echo "[mysqld]" | sudo tee -a $file > /dev/null
+    sudo echo "secure_file_priv=/var/lib/mysql-files/" | sudo tee -a $file > /dev/null
+    sudo service mysql restart
+    sudo sed -i "/\[mysqld\]/d" $file
+    sudo sed -i "/secure_file_priv=\/var\/lib\/mysql-files\//d" $file
+  fi
+
+  sudo cp ./fetdata/* /var/lib/mysql-files/
+  sed -i "0,/IZMIJENI/s//${1:0:2}${2:0:2}/" init.sql
+  sed -i "0,/IZMIJENI/s//${1:0:2}${2:0:2}/" init.sql
+  mysql --local-infile=1 -u root -p < init.sql
+  sed -i "0,/${1:0:2}${2:0:2}/s//IZMIJENI/" init.sql
+  sed -i "0,/${1:0:2}${2:0:2}/s//IZMIJENI/" init.sql
+  sudo rm /var/lib/mysql-files/FET_init_*.unl
+else	
+  echo "Broj argumenata nije validan (koristite . ./init.sh Ime Prezime)"
+fi
