0ce485c14f821ef960048254e3403a2fa2e11a8d
Mahir <mahir.suljic@fet.ba>
2024-10-21 20:34:38 +0200

    Vjezbe 2

--- Vjezbe/v2/priprema2.sql
+++ Vjezbe/v2/priprema2.sql
@@ -14,43 +14,40 @@ SELECT DISTINCT sifNastavnik FROM ispit WHERE sifPred = 146 AND ocjena = 1;
 SELECT mbrStud FROM ispit WHERE sifPred = 262 AND ocjena > 1;
 
 -- zadatak 6
-SELECT prezNastavnik, imeNastavnik, 800 * (koef + 0.4) AS PLATA FROM nastavnik;
+SELECT imeNastavnik, prezNastavnik, 800 * (koef + 0.4) AS plata FROM nastavnik;
 
 -- zadatak 7
-SELECT prezNastavnik FROM nastavnik WHERE 800 * (koef + 0.4) > 3500 AND 800 * (koef + 0.4) < 8000;
 SELECT prezNastavnik FROM nastavnik WHERE 800 * (koef + 0.4) BETWEEN 3500 AND 8000;
 
 -- zadatak 8
-SELECT prezNastavnik, SUBSTRING(imeNastavnik, 1, 1) AS ime FROM nastavnik;
-SELECT CONCAT(prezNastavnik, " ", SUBSTRING(imeNastavnik, 1, 1)) AS prezime_slovo FROM nastavnik;
+SELECT prezNastavnik, SUBSTR(imeNastavnik, 1, 1) AS prvoSlovoImena FROM nastavnik;
 
 -- zadatak 9
-SELECT imeStud, prezStud, CONCAT(SUBSTRING(imeStud, 1, 1), ".", SUBSTRING(prezStud, 1, 1), ".") AS INICIJALI FROM stud;
+SELECT imeStud, prezStud, CONCAT(SUBSTR(imeStud, 1, 1), '.', SUBSTR(prezStud, 1, 1), '.') AS inicijali FROM stud;
 
 -- zadatak 10
 SELECT * FROM ispit WHERE MONTH(datIspit) = 7;
 
 -- zadatak 11
-SELECT * FROM ispit WHERE MONTH(datIspit) = 8 AND DAYNAME(datIspit) = "Wednesday";
+SELECT * FROM ispit WHERE WEEKDAY(datIspit) = 2 AND MONTH(datIspit) = 8;
 
 -- zadatak 12
-SELECT mbrStud, ocjena, datIspit, CURDATE() AS danasnji, DATEDIFF(CURDATE(), datIspit) AS razlika FROM ispit;
-SELECT mbrStud, ocjena, datIspit, CURRENT_DATE AS danasnji, DATEDIFF(CURRENT_DATE, datIspit) AS razlika FROM ispit;
+SELECT mbrStud, ocjena, datIspit, CURRENT_DATE AS danasnjiDatum, DATEDIFF(CURRENT_DATE, datIspit) AS protekloDana FROM ispit;
 
 -- zadatak 13
-SELECT * FROM stud WHERE datRodStud = SUBDATE(CURDATE(), INTERVAL 25 YEAR);
+SELECT * FROM stud WHERE datRodStud = DATE_SUB(CURRENT_DATE, INTERVAL 25 YEAR);
 
 -- zadatak 14
-SELECT sifNastavnik, mbrStud, ocjena, DATEDIFF("2009-01-01", datIspit) AS protekloDana FROM ispit;
+SELECT sifNastavnik, mbrStud, ocjena, DATEDIFF(CURRENT_DATE, "2009-01-01") AS protekloDana FROM ispit;
 
 -- zadatak 15
-SELECT mbrStud, sifPred, ocjena, ADDDATE(datIspit, 3800) AS DATUM FROM ispit WHERE ADDDATE(datIspit, 3800) < CURDATE();
+SELECT mbrStud, sifPred, ocjena, datIspit, DATE_ADD(datIspit, INTERVAL 4800 DAY) AS 3800danaIspita FROM ispit WHERE DATE_ADD(datIspit, INTERVAL 4800 DAY) >= CURRENT_DATE;
 
 -- zadatak 16
-SELECT mbrStud, sifPred, ocjena, datIspit, ADDDATE(datIspit, INTERVAL 3 YEAR) AS datum_3 FROM ispit;
+SELECT mbrStud, sifPred, ocjena, datIspit, DATE_ADD(datIspit, INTERVAL 3 YEAR) AS 3godIspita FROM ispit;
 
 -- zadatak 17
 SELECT oznDvorana, oznVrstaDan, sat, sifPred AS predmet FROM rezervacija;
 
 -- zadatak 18
-SELECT pbrRod, pbrStan FROM stud;
+SELECT DISTINCT pbrRod, pbrStan FROM stud;
